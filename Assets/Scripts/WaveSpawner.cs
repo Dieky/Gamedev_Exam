35f61f8e7d14606f462d8ed6810ff55776c65c2c
Patrick <test@test.com>
2021-06-10 01:40:43 +0200

    redid most of the game and got to where i was. Added turrets that rotate at targets and switches targets

---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves = 5f;
+    private float countdown = 2f;
+    private float gold = 0f;
+
+    private float hitpoints = 10f;
+
+    public Text waveCountdownText;
+    public Text GoldAmount;
+    public Text LivessAmount;
+
+    private int waveIndex = 0;
+    void Update()
+    {
+        if (countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+        countdown -= Time.deltaTime;
+        waveCountdownText.text = Mathf.Round(countdown).ToString();
+        gold += Time.deltaTime;
+        GoldAmount.text = Mathf.Round(gold).ToString();
+        LivessAmount.text = hitpoints.ToString();
+    }
+
+    IEnumerator SpawnWave()
+    {
+        waveIndex++;
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
